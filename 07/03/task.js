b60e0377175bb3359b037223a9a46da39d19e9cb
GT017 <gt_017@mail.ru>
2017-11-09 23:37:53 +0300

    lesson-07

--- 07/03/task.js
+++ 07/03/task.js
@@ -3,8 +3,17 @@
  * Доп. задание: предложите несколько вариантов решения.
  */
 function throttle(time, callback) {
-  return callback;
+  let state = false;
+  return function() {
+    if (state) {
+      return;
+    }
+    callback.apply(this, arguments);
+    state = true;
+    setTimeout(function() {
+      state = false;
+    }, time);
+  }
 }
-
 
 module.exports = { throttle };
